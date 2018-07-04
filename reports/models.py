7b41deb8e76e7b8acab8068ee3df9c761dd59ed6
Hillel Arnold <helrond@hotmail.com>
2018-07-04 12:23:48 -0400

    building out models

--- reports/models.py
+++ reports/models.py
@@ -2,22 +2,49 @@ from django.db import models
 
 
 class Report(models.Model):
-    created_time = models.DateTimeField(auto_now_add=True)
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True, blank=True)
     PROCESS_STATUS_CHOICES = (
         ("queued", "Report queued"),
         ("started", "Report started"),
         ("completed", "Report completed"),
     )
     process_status = models.CharField(choices=PROCESS_STATUS_CHOICES, max_length=100)
+    items_checked = models.PositiveIntegerField(default=0)
+
+
+class ReportItem(models.Model):
+    file = models.CharField(max_length=512)
+    uri = models.CharField(max_length=512)
+    created = models.DateTimeField(auto_now_add=True)
 
 
 class FixityReport(Report):
 
     def run(self):
         print("Running {}".format(self))
+        # get each file
+        # calculate checksum and compare to stored checksum
+        # if the checksums don't match, create a report item
+        # increment items_checked
+
+
+class FixityReportItem(models.Model):
+    report = models.ForeignKey(FixityReport, on_delete=models.CASCADE, related_name="report")
+    stored_checksum = models.CharField(max_length=512)
+    calculated_checksum = models.CharField(max_length=512)
 
 
 class FormatReport(Report):
 
     def run(self):
         print("Running {}".format(self))
+        # get each file
+        # get format and uri
+        # create report item
+        # increment items checked
+
+
+class FormatReportItem(models.Model):
+    report = models.ForeignKey(FormatReport, on_delete=models.CASCADE, related_name="report")
+    file_format = models.CharField(max_length=256)
--- reports/serializers.py
+++ reports/serializers.py
@@ -1,16 +1,32 @@
 from rest_framework import serializers
-from reports.models import FixityReport, FormatReport
+from reports.models import FixityReport, FormatReport, FixityReportItem, FormatReportItem
+
+
+class FixityReportItemSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = FixityReportItem
+        fields = '__all__'
 
 
 class FixityReportSerializer(serializers.HyperlinkedModelSerializer):
+    errors = FixityReportItemSerializer(source="report", many=True)
 
     class Meta:
         model = FixityReport
+        fields = ('url', 'start_time', 'end_time', 'process_status', 'items_checked', 'errors')
+
+
+class FormatReportItemSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = FormatReportItem
         fields = '__all__'
 
 
 class FormatReportSerializer(serializers.HyperlinkedModelSerializer):
+    files = FormatReportItemSerializer(source="report", many=True)
 
     class Meta:
         model = FormatReport
-        fields = '__all__'
+        fields = ('url', 'start_time', 'end_time', 'process_status', 'items_checked', 'files')
