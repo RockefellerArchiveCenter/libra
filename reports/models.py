2c4a8415ccd2c9df9cb7e8104f706e43528e0a00
Hillel Arnold <helrond@hotmail.com>
2018-10-11 16:15:19 -0400

    factor model methods into separate class

--- reports/models.py
+++ reports/models.py
@@ -1,11 +1,4 @@
-from urllib.parse import urljoin
-
 from django.db import models
-from django.utils import timezone
-
-from libra import config as CF
-
-from reports.clients import FedoraClient
 
 
 class Report(models.Model):
@@ -13,6 +6,11 @@ class Report(models.Model):
     queued_time = models.DateTimeField(null=True, blank=True) # Datetime the report should be started
     start_time = models.DateTimeField(null=True, blank=True) # Datetime the report was started
     end_time = models.DateTimeField(null=True, blank=True) # Datetime the report finished
+    REPORT_TYPE_CHOICES = (
+        ("fixity", "Fixity"),
+        ("format", "Format"),
+    )
+    report_type = models.CharField(choices=REPORT_TYPE_CHOICES, max_length=100)
     PROCESS_STATUS_CHOICES = (
         ("queued", "Report queued"),
         ("started", "Report started"),
@@ -28,74 +26,11 @@ class ReportItem(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
 
-class FixityReport(Report):
-
-    def get_type(self): return 'fixity'
-
-    def process_children(self, resources):
-        for r in resources:
-            if r.__class__.__name__ == 'NonRDFSource':
-                print("Creating report item for {}".format(r.uri))
-                fixity = r.fixity()
-                FixityReportItem.objects.create(
-                    report=self,
-                    verdict=fixity['verdict'],
-                    uri=r.uri,
-                )
-                self.items_checked += 1
-            if len(r.children()) > 0:
-                self.process_children(r.children(as_resources=True))
-
-    def run(self):
-        self.client = FedoraClient()
-        print("Running {}".format(self))
-        self.process_status = 'started'
-        self.start_time = timezone.now()
-        self.save()
-        base_resource = self.client.get_resource(CF.FEDORA['baseurl'])
-        children = base_resource.children(as_resources=True)
-        self.process_children(children)
-        self.process_status = 'completed'
-        self.end_time = timezone.now()
-        self.save()
-
-
 class FixityReportItem(ReportItem):
-    report = models.ForeignKey(FixityReport, on_delete=models.CASCADE, related_name="report")
+    report = models.ForeignKey(Report, on_delete=models.CASCADE, related_name="fixity_report")
     verdict = models.CharField(max_length=10)
 
 
-class FormatReport(Report):
-
-    def get_type(self): return 'format'
-
-    def process_children(self, resources):
-        for r in resources:
-            if r.__class__.__name__ == 'NonRDFSource':
-                print("Creating report item for {}".format(r.uri))
-                FormatReportItem.objects.create(
-                    report=self,
-                    uri=r.uri,
-                    file_format=r.binary.mimetype,
-                )
-                self.items_checked += 1
-        if len(r.children()) > 0:
-            self.process_children(r.children(as_resources=True))
-
-    def run(self):
-        self.client = FedoraClient()
-        print("Running {}".format(self))
-        self.process_status = 'started'
-        self.start_time = timezone.now()
-        self.save()
-        base_resource = self.client.get_resource(CF.FEDORA['baseurl'])
-        children = base_resource.children(as_resources=True)
-        self.process_children(children)
-        self.process_status = 'completed'
-        self.end_time = timezone.now()
-        self.save()
-
-
 class FormatReportItem(ReportItem):
-    report = models.ForeignKey(FormatReport, on_delete=models.CASCADE, related_name="report")
+    report = models.ForeignKey(Report, on_delete=models.CASCADE, related_name="format_report")
     file_format = models.CharField(max_length=256)
--- reports/tests.py
+++ reports/tests.py
@@ -9,7 +9,7 @@ from rest_framework.test import APIRequestFactory
 
 from libra import settings
 from reports.cron import RunReports
-from reports.models import FixityReport, FormatReport, FixityReportItem, FormatReportItem
+from reports.models import Report, FixityReportItem, FormatReportItem
 from reports.views import FixityReportViewSet, FormatReportViewSet
 
 reports_vcr = vcr.VCR(
@@ -42,17 +42,16 @@ class ReportTest(TestCase):
             response = FixityReportViewSet.as_view(actions={"post": "create"})(request)
             print('Created fixity report {url}'.format(url=response.data['url']))
             self.assertEqual(response.status_code, 200, "Wrong HTTP code")
-        self.assertEqual(len(FixityReport.objects.all()), self.fixity_report_count, "Wrong number of fixity reports created")
-        self.assertEqual(len(FormatReport.objects.all()), self.format_report_count, "Wrong number of format reports created")
-        for report_type in [FixityReport.objects.all(), FormatReport.objects.all()]:
-            for report in report_type:
-                self.assertEqual(report.process_status, 'queued', "Report process status not correctly set")
-                self.assertIsNot(report.start_time, False, "Report start time not added")
+        self.assertEqual(len(Report.objects.filter(report_type='fixity')), self.fixity_report_count, "Wrong number of fixity reports created")
+        self.assertEqual(len(Report.objects.filter(report_type='format')), self.format_report_count, "Wrong number of format reports created")
+        for report in Report.objects.all():
+            self.assertEqual(report.process_status, 'queued', "Report process status not correctly set")
+            self.assertIsNot(report.start_time, False, "Report start time not added")
 
     def create_report_items(self):
         print('*** Creating Report Items ***')
-        format_report = random.choice(FormatReport.objects.all())
-        fixity_report = random.choice(FixityReport.objects.all())
+        format_report = random.choice(Report.objects.filter(report_type='format'))
+        fixity_report = random.choice(Report.objects.filter(report_type='fixity'))
         for i in range(self.format_report_items_count):
             FormatReportItem.objects.create(
                 report=format_report,
@@ -67,23 +66,22 @@ class ReportTest(TestCase):
                 uri=''.join(random.choices(string.ascii_letters + string.digits, k=20)),
                 verdict=random.choice([True, False]),
             )
-        self.assertEqual(len(FixityReport.objects.all()), self.fixity_report_count, "Wrong number of fixity reports created")
-        self.assertEqual(len(FormatReport.objects.all()), self.format_report_count, "Wrong number of format reports created")
+        self.assertEqual(len(Report.objects.filter(report_type='fixity')), self.fixity_report_count, "Wrong number of fixity reports created")
+        self.assertEqual(len(Report.objects.filter(report_type='format')), self.format_report_count, "Wrong number of format reports created")
 
     def run_reports(self):
         print('*** Running Reports ***')
         with reports_vcr.use_cassette('run_reports.yml'):
             RunReports().do()
-        for report_type in [FixityReport.objects.all(), FormatReport.objects.all()]:
-            for report in report_type:
-                self.assertEqual(report.process_status, 'completed', 'Report was not completed')
-                self.assertNotEqual(report.start_time, None, 'Start time not added')
-                self.assertNotEqual(report.end_time, None, 'End time not added')
+        for report in Report.objects.all():
+            self.assertEqual(report.process_status, 'completed', 'Report was not completed')
+            self.assertNotEqual(report.start_time, None, 'Start time not added')
+            self.assertNotEqual(report.end_time, None, 'End time not added')
 
     def delete_reports(self):
         print('*** Deleting Reports ***')
-        format_report = random.choice(FormatReport.objects.all())
-        fixity_report = random.choice(FixityReport.objects.all())
+        format_report = random.choice(Report.objects.filter(report_type='format'))
+        fixity_report = random.choice(Report.objects.filter(report_type='fixity'))
         request = self.factory.delete(reverse('fixityreport-detail', kwargs={'pk': fixity_report.pk}), format='json')
         response = FixityReportViewSet.as_view(actions={"delete": "destroy"})(request, pk=fixity_report.pk)
         print('Deleted fixity report')
@@ -92,8 +90,8 @@ class ReportTest(TestCase):
         response = FormatReportViewSet.as_view(actions={"delete": "destroy"})(request, pk=format_report.pk)
         print('Deleted format report')
         self.assertEqual(response.status_code, 204, "Wrong HTTP code")
-        self.assertEqual(len(FixityReport.objects.all()), self.fixity_report_count-1, "Wrong number of fixity reports created")
-        self.assertEqual(len(FormatReport.objects.all()), self.format_report_count-1, "Wrong number of format reports created")
+        self.assertEqual(len(Report.objects.filter(report_type='fixity')), self.fixity_report_count-1, "Wrong number of fixity reports created")
+        self.assertEqual(len(Report.objects.filter(report_type='format')), self.format_report_count-1, "Wrong number of format reports created")
 
     def test_reports(self):
         self.create_reports()
